0c59546d60a9139368cf89e6d33b421d7eb79a9c
val <valeria@gmail.com>
2024-05-26 17:37:32 -0500

    agg datos de los vinos al json y le di formato al archivo de register

--- js/register.js
+++ js/register.js
@@ -1,93 +1,97 @@
-// import Swal from 'sweetalert2'
+import Swal from 'sweetalert2'
 
 //Const creation for Dom elements
-const userName = document.querySelector("#name")
-const lastName = document.querySelector("#userlastname")
-const phone = document.querySelector("#phone")
-const email = document.querySelector("#email")
-const password = document.querySelector("#password")
-const formRegister = document.querySelector("#form_register")
-const URL = "http://localhost:3000/users"
-
+const userName = document.querySelector("#name");
+const lastName = document.querySelector("#userlastname");
+const phone = document.querySelector("#phone");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const formRegister = document.querySelector("#form_register");
+const URL = "http://localhost:3000/users";
 
 //Creaction of event to set user register
 formRegister.addEventListener("submit", (event) => {
-    event.preventDefault()
-    setJSONserver()
-})
+  event.preventDefault();
+  setJSONserver();
+});
 
 //asyn function to connect to json.server
 let setJSONserver = async () => {
-    let newUserName = userName.value.trim()//delete espaces before and after value
-    let newLastName = lastName.value.trim()
-    let newPhone = phone.value.trim()
-    let newEmail = email.value.trim()
-    let newPassword = password.value.trim()
+  let newUserName = userName.value.trim(); //delete espaces before and after value
+  let newLastName = lastName.value.trim();
+  let newPhone = phone.value.trim();
+  let newEmail = email.value.trim();
+  let newPassword = password.value.trim();
 
-    if (newUserName === "" || newLastName === "" || newPhone === "" || newPassword === "") {
-        alert("Por favor, completa todos los campos.");
-    } else {
-        // Si todos los campos están completos, puedes enviar el formulario
-        let data = await fetch (URL)
-        let dataString = await data.json()
-        let send = true;
+  if (
+    newUserName === "" ||
+    newLastName === "" ||
+    newPhone === "" ||
+    newPassword === ""
+  ) {
+    alert("Por favor, completa todos los campos.");
+  } else {
+    // Si todos los campos están completos, puedes enviar el formulario
+    let data = await fetch(URL);
+    let dataString = await data.json();
+    let send = true;
 
-         dataString.forEach(element => {
-            if(element.newEmail === newEmail){
-                Swal.fire({
-                    icon: "error",
-                    title: "Oops...",
-                    text: "Ya hay un usuario registrado con este Email",
-                  });
-                send = false
-            }
-            if(element.newPhone === newPhone){
-                Swal.fire({
-                    icon: "error",
-                    title: "Oops...",
-                    text: "Ya hay un usuario registrado con este telefono",
-                  });
-                send = false
-            }
+    dataString.forEach((element) => {
+      if (element.newEmail === newEmail) {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Ya hay un usuario registrado con este Email",
         });
+        send = false;
+      }
+      if (element.newPhone === newPhone) {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Ya hay un usuario registrado con este telefono",
+        });
+        send = false;
+      }
+    });
 
-        if (send){
-           let objectRegister = {
-            newUserName,
-            newLastName,
-            newPhone,
-            newEmail,
-            newPassword
-        }
-       
-        await register(objectRegister)
+    if (send) {
+      let objectRegister = {
+        newUserName,
+        newLastName,
+        newPhone,
+        newEmail,
+        newPassword,
+      };
 
-        const Toast = Swal.mixin({
-            toast: true,
-            position: "top-end",
-            showConfirmButton: false,
-            timer: 3000,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-              toast.onmouseenter = Swal.stopTimer;
-              toast.onmouseleave = Swal.resumeTimer;
-            }
-          });
-          Toast.fire({
-            icon: "success",
-            title: "Signed in successfully"
-          });
-        }      
-        }
-//    formRegister.reset()
-}
+      await register(objectRegister);
+
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        },
+      });
+      Toast.fire({
+        icon: "success",
+        title: "Signed in successfully",
+      });
+    }
+  }
+  //    formRegister.reset()
+};
 
 let register = async (objectRegister) => {
-    await fetch (URL, {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(objectRegister)
-    })
-}
+  await fetch(URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(objectRegister),
+  });
+};
