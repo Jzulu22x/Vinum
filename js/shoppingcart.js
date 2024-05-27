69801a7ac3660fcc2f3fdbe6a1d7a3bc6eb6d243
Juan Diego <juanzr1015@gmail.com>
2024-05-27 17:28:38 -0500

    Shopping cart Function

--- js/shoppingcart.js
+++ js/shoppingcart.js
@@ -1,28 +1,26 @@
-document.querySelector(".btn-plus").addEventListener("click", sumaruno);
-document.querySelector(".btn-rest").addEventListener("click", restaruno);
-document.querySelector(".btn-trash").addEventListener("click",borrarContenido);
+const divShoppingCart = document.querySelector("main")
 
-let contador = 0;
-let precio = 0;
-
-function sumaruno() {
-    contador = contador + 1;
-    precio = contador *290.99 ; 
-    document.querySelector("#msgcontador").innerHTML = contador;
-    document.querySelector("#price_desktop").innerHTML = precio;
+let getCartElements = async () => {
+    let getElements = await fetch ("http://localhost:3000/cart")
+    let getElementsCode = await getElements.json()
+    getElementsCode.forEach(element => {
+        divShoppingCart.innerHTML += `
+        <div id="container_product_desktop">
+        <img src="${element.image}" alt="">
+        <div id="squeare_desciption_desktop">
+            <h2>${element.name}</h2>
+            <div id="button_pay_desktop">
+                <h5>${element.price}</h5>
+                <button>
+                    <box-icon name='trash' size="25px" type='solid' color='#ffffff' ></box-icon>
+                    <box-icon name='plus-circle' size="25px" color='#ffffff' ></box-icon>
+                    <p>1</p>
+                    <box-icon name='minus-circle' size="25px" color='#ffffff' ></box-icon>
+                </button>
+            </div>
+        </div> 
+      </div>`
+    })
 }
 
-function restaruno() {
-    if (contador > 0) {
-        contador = contador - 1;
-        precio = contador * 290.99 ; 
-        document.querySelector("#msgcontador").innerHTML = contador;
-        document.querySelector("#price_desktop").innerHTML = precio;
-    }
-}
-function borrarContenido() {
-    document.querySelector("container_product_desktop").innerHTML = "";
-    document.querySelector("#price_desktop").innerHTML = "";
-    contador = 0;
-    precio = 0;
-}
+getCartElements()
