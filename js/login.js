2eb9e7b224d0eb96a75f470407ee8b7dc74ccddb
vanhallen46 <salamancadiego46@gmail.com>
2024-05-30 07:54:56 -0500

    se agregan los comentarios del codigo

--- js/login.js
+++ js/login.js
@@ -1,26 +1,31 @@
 const URL_USERS = "http://localhost:3000/users";
 
-// selectores
+// selectors
 
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 const formLogin = document.querySelector("#formLogin");
 
+// Listener event submit form
 formLogin.addEventListener("submit", async (event) => {
   event.preventDefault();
   await login();
   console.log("submit");
 });
 
+// Login Function
 async function login() {
+  // API request
   const response = await fetch(`${URL_USERS}?newEmail=${email.value}`);
+  // Transform the response to JSON
   const data = await response.json();
 
+  // Email verification
   if (data.length === 0) {
     console.error("EMAIL NO ENCONTRADO");
     return;
   }
-
+  // Pasword verification
   if (data[0].newPassword === password.value) {
     console.log("PASSWORD VÁLIDO");
   } else {
@@ -29,6 +34,8 @@ async function login() {
     return;
   }
 
+  // Store user data in local Storage
   localStorage.setItem("usuario", JSON.stringify(data));
+  // Redirect to Homepage
   window.location.href = "/html/homePage.html";
 }
